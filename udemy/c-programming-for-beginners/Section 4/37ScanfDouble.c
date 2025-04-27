9048152f2130f50f530c50cc81c4e62d12c19214
Bill6622 <yumesaki6622@yahoo.co.jp>
2025-04-27 15:35:23 +0900

    Some new additions are pushed.

---
+++ udemy/c-programming-for-beginners/Section 4/37ScanfDouble.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  char str[100];
+  double x;
+
+  printf("Please enter double number.\n");
+  scanf("%lf", &x);
+  printf("You entered double number: %lf\n", x);
+
+  return 0;
+}
---
+++ udemy/c-programming-for-beginners/Section 4/37ScanfInteger.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  char str[100];
+  int i;
+
+  printf("Please enter integer number.\n");
+  scanf("%d", &i);
+  printf("You entered integer number: %d\n", i);
+
+  return 0;
+}
